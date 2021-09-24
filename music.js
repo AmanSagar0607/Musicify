13e6a43e1e3c7372e4ec6920ca519107bd2a1704
luckysagar6265 <62849475+luckysagar6265@users.noreply.github.com>
2021-09-24 15:06:39 +0530

    Update music.js

--- music.js
+++ music.js
@@ -1,41 +1,39 @@
-
-        const music = document.querySelector("audio");
-        const play = document.getElementById("play");
-        const img = document.querySelector("img");
-
-        const title = document.getElementById("title");
-        const artist = document.getElementById("artist");
-        const prev = document.getElementById("prev");
-        const next = document.getElementById("next");
+const music = document.querySelector("audio");
+const play = document.getElementById("play");
+const img = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
 
 
 
 
-        let isplaying = false;
+let isplaying = false;
 
-        //play function
-        const playMusic = () => {
-            isplaying = true;
-            music.play();
-            play.classList.replace('fa-play', "fa-pause");
-            img.classList.add("anime");
-        };
+//play function
+const playMusic = () => {
+    isplaying = true;
+    music.play();
+    play.classList.replace('fa-play', "fa-pause");
+    img.classList.add("anime");
+};
 
-        //pause function
-        const pauseMusic = () => {
-            isplaying = false;
-            music.pause();
-            play.classList.replace('fa-pause', "fa-play");
-            img.classList.remove("anime");
-        };
+//pause function
+const pauseMusic = () => {
+    isplaying = false;
+    music.pause();
+    play.classList.replace('fa-pause', "fa-play");
+    img.classList.remove("anime");
+};
 
-        play.addEventListener('click', () => {
-            if (isplaying) {
-                pauseMusic();
-            } else {
-                playMusic();
-            }
-        });
+play.addEventListener('click', () => {
+    if (isplaying) {
+        pauseMusic();
+    } else {
+        playMusic();
+    }
+});
 
 
 
@@ -43,55 +41,50 @@
 
 
 
-        const songs = [
-            {
-                name: "music-1",
-                title: "See You Again",
-                artist: "Wiz Khalifa",
-            },
-            {
-                name: "music-2",
-                title: "Girls Like You",
-                artist: "Maroon 5",
-            },
-            {
-                name: "music-3",
-                title: "Ma Belle",
-                artist: "AP Dhillon",
-            },
-            {
-                name: "music-4",
-                title: "Astronaut",
-                artist: "Masked Wolf",
-            },
-        ];
+const songs = [{
+        name: "music-1",
+        title: "See You Again",
+        artist: "Wiz Khalifa",
+    },
+    {
+        name: "music-2",
+        title: "Girls Like You",
+        artist: "Maroon 5",
+    },
+    {
+        name: "music-3",
+        title: "Ma Belle",
+        artist: "AP Dhillon",
+    },
+    {
+        name: "music-4",
+        title: "Astronaut",
+        artist: "Masked Wolf",
+    },
+];
 
 
-        //  isplaying ? pauseMusic() : playMusic();
 
+//changing music data
+const loadSong = (songs) => {
+    title.textContent = songs.title;
+    artist.textContent = songs.artist;
+    music.src = "./music/" + songs.name + ".mp3";
+    img.src = "images/" + songs.name + ".jpg";
+};
 
-        //changing music data
-        const loadSong = (songs) => {
-            title.textContent = songs.title;
-            artist.textContent = songs.artist;
-            music.src = "./music/" + songs.name + ".mp3";
-            img.src = "images/" + songs.name + ".jpg";
-        };
+songIndex = 0;
+const nextsong = () => {
+    songIndex = (songIndex + 1) % songs.length;
+    loadSong(songs[songIndex]);
+    playMusic();
+};
 
-        // loadSong(songs[2]); 
-        songIndex = 0;
-        const nextsong = () => {
-            songIndex = (songIndex +1) % songs.length;
-            loadSong(songs[songIndex]);
-            playMusic();
-        };
+const prevSong = () => {
+    songIndex = (songIndex - 1) % songs.length;
+    loadSong(songs[songIndex]);
+    playMusic();
+}
 
-        const prevSong= () =>{
-            songIndex = (songIndex -1) % songs.length;
-            loadSong(songs[songIndex]);
-            playMusic();
-        }  
-
-        next.addEventListener('click', nextsong);
-        prev.addEventListener('click', prevSong);
-        
+next.addEventListener('click', nextsong);
+prev.addEventListener('click', prevSong);
